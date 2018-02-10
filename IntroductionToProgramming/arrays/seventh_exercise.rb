4b9b668c1050314f72df41726c2fd954e2a792ea
EricRicketts <eric_ricketts@me.com>
2018-02-10 06:52:58 -0500

    correction to last exercise

--- IntroductionToProgramming/arrays/seventh_exercise.rb
+++ IntroductionToProgramming/arrays/seventh_exercise.rb
@@ -5,8 +5,8 @@ class SeventhExercise < Minitest::Test
 
   def test_muliply_by_two
     arr = (1..10).to_a
-    expected = (2..20).step(2).to_a
-    result = arr.map { |num| num * 2 }
+    expected = (3..12).to_a
+    result = arr.map { |num| num + 2 }
     assert_equal(expected, result)
   end
   
