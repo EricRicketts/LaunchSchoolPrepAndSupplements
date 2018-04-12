85481933602a16ee273fb0a0032596e0a087e2be
EricRicketts <eric_ricketts@me.com>
2018-04-12 17:09:58 -0400

    exercises four, five, and six of Introduction To Regular Expressions

--- IntroductionToRegularExpressions/character-classes/exercises.rb
+++ IntroductionToRegularExpressions/character-classes/exercises.rb
@@ -28,5 +28,26 @@ class Exercises < Minitest::Test
     my_regex = Regexp.new(/[0-9A-Ja-j]/)
     assert_equal(28, str.scan(my_regex).length)
   end
+
+  def test_exercise_four_and_five
+    str = "0x1234" + "\n" +
+      "Too many XXXXXXXXXXxxxxxxXXXXXXXXXXXX to count." + "\n" +
+      "The quick brown fox jumps over the lazy dog" + "\n" +
+      "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"
+    my_regex = Regexp.new(/[a-wyzA-WYZ]/)
+    assert_equal(82, str.scan(my_regex).length)
+    # [^xX] will not work because characters other than
+    # letters will be matched, the exercise only class
+    # for all letters to be matched that are not x or X
+  end
+
+  def test_exercise_six
+    str = "0x1234abcd" + "\n" +
+      "1,000,000,000s and 1,000,000,000s." + "\n" +
+      "THE quick BROWN fox JUMPS over THE lazy DOG!"
+    my_regex = Regexp.new(/[^A-Za-z]/)
+    assert([45, 46].include?(str.scan(my_regex).length))
+    # a better regex would be /[^a-z]/i
+  end
   
 end
