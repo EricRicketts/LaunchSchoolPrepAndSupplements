93ea3d5a2773db177f63e85f8671134f1722910e
EricRicketts <eric_ricketts@me.com>
2018-04-17 05:44:21 -0400

    second exercise in using regular expressions chapter of Introduction To Regular Expressions

--- IntroductionToRegularExpressions/using-regular-expressions/javascript/test/exercises.test.js
+++ IntroductionToRegularExpressions/using-regular-expressions/javascript/test/exercises.test.js
@@ -9,12 +9,26 @@ describe('Exercises for Using Regular Expressions Chapter in Introduction To Reg
         return regex.test(str);
     }
 
+    function fields(str) {
+        return str.split(/\W+/);
+    }
+
     it('testing URLs', function () {
         var urls = ['http://launchschool.com', 'https://example.com',
             'https://example.com hello', '   https://example.com'];
-        var results = urls.map(isURL);
         var expected = [true, true, false, false]
-        expect(expected).to.deep.equal(results);
+        expect(expected).to.deep.equal(urls.map(isURL));
+    });
+
+    it('testing fields', function () {
+        var strs = ["Pete,201,Student", "Pete \t 201    ,  TA",
+            "Pete \t 201"];
+        var expected = [
+            ["Pete", "201", "Student"],
+            ["Pete", "201", "TA"],
+            ["Pete", "201"]
+        ];
+        expect(expected).to.deep.equal(strs.map(fields));
     });
     
 });
--- IntroductionToRegularExpressions/using-regular-expressions/ruby/exercises.rb
+++ IntroductionToRegularExpressions/using-regular-expressions/ruby/exercises.rb
@@ -8,11 +8,26 @@ class Exercises < Minitest::Test
     str.match?(/\Ahttps?:\/\/\S+\z/)
   end
 
+  def fields(str)
+    str.split(/\W+/)
+  end
+
   def test_url
     urls = ['http://launchschool.com', 'https://example.com',
       'https://example.com hello', '   https://example.com']
     expected = [true, true, false, false]
     assert_equal(expected, urls.map {|str| url?(str)})
+  end
+
+  def test_fields
+    strs = ["Pete,201,Student", "Pete \t 201    ,  TA",
+      "Pete \t 201"]
+    expected = [
+      ["Pete", "201", "Student"],
+      ["Pete", "201", "TA"],
+      ["Pete", "201"]
+    ]
+    assert_equal(expected, strs.map {|str| fields(str)})
   end
   
 end
